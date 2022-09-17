f43ffc7ec17773fcba4cfa845a61d96f61ef5667
Kabingu Sammy <80510478+kabingusam@users.noreply.github.com>
2022-09-17 12:05:23 +0300

    update

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,10 @@
 # Create a new file
+
 file = {'/tmp/school':
-ensure => file,
-permission => '0774',
-owner => 'www-data',
-group => 'www-data',
-content => 'I love Puppet',
+    ensure  => 'present',
+    mode    => '0774',
+    owner   => 'www-data',
+    group   => 'www-data',
+    content => 'I love Puppet',
+    path    => '/tmp/school',
 }
